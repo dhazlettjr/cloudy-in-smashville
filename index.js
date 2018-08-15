83dd8321fba8fe2eeabeb919ed923db91806e790
dhazlettjr <d.hazlett.jr@gmail.com>
2018-08-14 21:30:03 -0500

    loaded the api key, created the request

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+
+// require the request package
+//Once you pass in a url, the request package returns a callback function with three arguments
+
 const request = require('request');
+
+//create a variable for the actualy key, the city and the url of the api request site
+let apiKey = '7646dc8cdac6461b5b997f33f0a9c975';
+let city = 'nashville';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
 request(url, function (err, response, body) {
   if(err){
     console.log('error:', error);
@@ -6,3 +16,4 @@ request(url, function (err, response, body) {
     console.log('body:', body);
   }
 });
+
